c101e2b8af1b2543f64a4d8bb9a7cb82b9f9c87e
pschultzendorff <peter.schultzendorff@uib.no>
2026-03-04 13:13:45 +0000

    MAINT: Fix package version numbers in `setup.py`

--- setup.py
+++ setup.py
@@ -4,9 +4,23 @@ setup(
     name="hc_buckleyleverett",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["jax", "matplotlib", "PyQt6", "scikit-image", "seaborn", "tqdm"],
+    install_requires=[
+        "jax==0.9.0.1",
+        "matplotlib==3.10.8",
+        "PyQt6==6.10.2",
+        "scikit-image==0.26.0",
+        "seaborn==0.13.2",
+        "tqdm==4.67.3",
+    ],
     extras_require={
-        "dev": ["mypy", "pytest", "ruff", "types-tqdm", "types-seaborn", "scipy-stubs"],
+        "dev": [
+            "mypy==1.19.1",
+            "pytest==9.0.2",
+            "ruff==0.15.2",
+            "types-tqdm==4.67.3.20260205",
+            "types-seaborn==0.13.2.20251221",
+            "scipy-stubs==1.17.1.0",
+        ],
     },
     author="Peter von Schultzendorff",
     author_email="peter.schultzendorff@uib.no",
@@ -18,5 +32,5 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.12",
+    python_requires="==3.12.12",
 )
